6102c0704da08eddd5125c2c1fc4c58f17224064
Juliana <juliana.hooson16@gmail.com>
2022-11-30 18:53:31 -0800

    julesPageObject.ts

---
+++ julesPageObject.ts
@@ -0,0 +1,18 @@
+import {By} from "selenium-webdriver"
+import {BasePage} from './basePage'
+
+export class PetStore extends BasePage {
+    CatCategory: By = By.xpath('//a[@href="/actions/Catalog.action?viewItem=&amp;itemId=EST-16"]');
+    PersianCat: By = By.xpath('//a[@href="/actions/Catalog.action?viewProduct=&productId=FL-DLH-02"]');
+    AdultFemaleCatPersianImage: By=By.xpath('//img[@src="../images/cat1.gif"]');
+    returnToCatCategory: By = By.xpath('//a[@href="/actions/Catalog.action?viewProduct=&productId=FL-DLH-02"]');
+    returnToCats: By = By.xpath('//a[@href="/actions/Catalog.action?viewCategory=&categoryId=CATS"]');
+    returnToHomePage: By = By.xpath('//div[@id="Main"]')
+
+    constructor() {
+        super ({url:"https://petstore.octoperf.com/actions/Catalog.action"});
+    };
+    async getResults() {
+        return await this.getText(this.returnToHomePage);
+    };
+}
