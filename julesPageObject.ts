7061dbbd12453899275fe6d14f314c0b50523479
Juliana <juliana.hooson16@gmail.com>
2022-11-30 19:27:03 -0800

    jules first part

--- julesPageObject.ts
+++ julesPageObject.ts
@@ -2,6 +2,7 @@ import {By} from "selenium-webdriver"
 import {BasePage} from './basePage'
 
 export class PetStore extends BasePage {
+    HomePage: By = By.xpath('//a[@href="/actions/Catalog.action?viewCategory=&categoryId=CATS"]')
     CatCategory: By = By.xpath('//a[@href="/actions/Catalog.action?viewItem=&amp;itemId=EST-16"]');
     PersianCat: By = By.xpath('//a[@href="/actions/Catalog.action?viewProduct=&productId=FL-DLH-02"]');
     AdultFemaleCatPersianImage: By=By.xpath('//img[@src="../images/cat1.gif"]');
